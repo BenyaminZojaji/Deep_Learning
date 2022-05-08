19e7928c99752ffa0f9b5f9b585b9e33c846e6f3
Benyamin Zojaji <benyamin.zojaji@gmail.com>
2022-05-08 20:00:10 +0430

    add essential files

---
+++ PyTorch PersianMnist/inference.py
@@ -0,0 +1,49 @@
+import argparse
+import time
+import torch
+import torchvision
+from torchvision import transforms
+import cv2
+import numpy as np
+from model import Model
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--processor', type=str, default='cpu', help='cpu/cuda')
+parser.add_argument('--input', type=str, default='input.jpg', help='input PATH')
+parser.add_argument('--weights', type=str, default='weights.pth', help='weights PATH')
+args = parser.parse_args()
+
+transform = torchvision.transforms.Compose([
+        transforms.ToPILImage(), # added because of the Error: img should be PIL Image. Got <class 'numpy.ndarray'>
+        transforms.RandomRotation(10),
+        transforms.Resize((70, 70)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+])
+
+device = torch.device(args.processor)
+
+model = Model()
+model.load_state_dict(torch.load(args.weights))
+model = model.to(device)
+
+model.eval()
+
+# oreoricess
+img = cv2.imread('/content/pullover.png')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.resize(img, (28, 28))
+tensor = transform(img).unsqueeze(0).to(device)
+
+# process
+start = time.time()
+preds = model(tensor)
+end = time.time()
+
+# postprocess
+# _, output=torch.max(preds, 1)
+preds = preds.cpu().detach().numpy()
+output = np.argmax(preds)
+classes = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+print(f'{classes[output]}, Process-Time: {end-start}')
---
+++ PyTorch PersianMnist/model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Model(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.conv1 = nn.Conv2d(3, 32, (3, 3), (1, 1), (1, 1))
+    self.conv2 = nn.Conv2d(32, 32, (3, 3), (1, 1), (1, 1))
+    self.conv3 = nn.Conv2d(32, 64, (3, 3), (1, 1), (1, 1))
+    self.conv4 = nn.Conv2d(64, 64, (3, 3), (1, 1), (1, 1))
+
+    self.fc1 = nn.Linear(64*8*8, 512) # batch_size * size * size (size after convs and maxpoolings)
+    self.fc2 = nn.Linear(512, 10)
+
+  def forward(self, x):
+    x = F.relu(self.conv1(x))
+    x = F.max_pool2d(x, kernel_size=(2, 2))
+    x = F.relu(self.conv2(x))
+    x = F.max_pool2d(x, kernel_size=(2, 2))
+    x = F.relu(self.conv3(x))
+    x = F.max_pool2d(x, kernel_size=(2, 2))
+    x = F.relu(self.conv4(x))
+    x = torch.flatten(x, start_dim=1) # because we don't want to flatten all of it! (0 is batch size)
+    x = F.relu(self.fc1(x))
+    x = self.fc2(x)
+    x = torch.softmax(x, dim=1)
+    return x
